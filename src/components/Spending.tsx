c2a378c68557bbf1c4f99fa8e33d79b56b8e440b
scvang <scvang@iastate.edu>
2022-08-31 22:10:52 -0500

    bump

--- src/components/Spending.tsx
+++ src/components/Spending.tsx
@@ -10,13 +10,15 @@ export default class Spending extends React.Component{
 
     render(){
         return(
-            <Chart
-            chartType="PieChart"
-            data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-            width="100%"
-            height="400px"
-            legendToggle
-            />
+            <div>
+                <Chart
+                chartType="PieChart"
+                data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+                width="100%"
+                height="400px"
+                legendToggle
+                />
+            </div>
         );
     }
 }
